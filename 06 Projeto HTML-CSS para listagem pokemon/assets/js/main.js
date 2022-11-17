1926d1f95f4ee82c9ff88f74cfaf9afc16fb57a4
Felipe Marchi <fmg.17@outlook.com>
2022-11-17 13:32:08 -0300

    Update pokedex 17Nov22

--- 06 Projeto HTML-CSS para listagem pokemon/assets/js/main.js
+++ 06 Projeto HTML-CSS para listagem pokemon/assets/js/main.js
@@ -1,33 +1,29 @@
-function convertPokemonToLI(pokemon) {
-    return `
-    <li class="pokemon ${pokemon.type}">
-        <span class="number">#${pokemon.number}</span>
-        <span class="name">${pokemon.name}</span>
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5
+let offset = 0
 
-        <div class="detail">
-            <ol class="types">
-                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-            </ol>
-            <img src="${pokemon.photo}" alt="${pokemon.name}"></div>
-    </li>
-`
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(pokemon => {
+            return `<li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+        
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}"></div>
+            </li>`
+        }).join('')
+        pokemonList.innerHTML += newHtml
+    })
 }
 
-const pokemonList = document.getElementById('pokemonList')
+loadPokemonItens(offset, limit)
 
-// retorna uma promise - processamento assíncrono
-pokeApi.getPokemons().then((pokemons = []) => {
-
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLI).join('')
-
-    /**
-     * const listItems = []
-    for(let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i]
-        listItems.push(convertPokemonToLI(pokemon))
-    }
-    listItems.forEach(element => {
-        pokemonList.innerHTML += element
-    });
-     */
-}) 
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
+})
